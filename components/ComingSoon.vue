37a4fe95e160e43e56380d80f8d621802ac854a3
Mark Donatelli <markwdonatelli@yahoo.com>
2020-07-25 14:24:53 -0400

    coming soon component / page started

---
+++ components/ComingSoon.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Coming Soon...</h1>
+    <p>New portfolio launch!</p>
+    <p>Mark Donatelli - Front End Developer</p>
+    <p>Contact - markdonatelli3@gmail.com</p>
+  </div>
+</template>
+
+<style>
+div {
+  text-align: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="container-full-width">
-    <div class="row">
-      <header>
-        <ul>
-          <li><a href="home">Home</a></li>
-          <li><a href="home">About</a></li>
-          <li><a href="home">Work</a></li>
-        </ul>
-      </header>
+    <div class="inner">
+      <ComingSoon />
     </div>
   </div>
 </template>
