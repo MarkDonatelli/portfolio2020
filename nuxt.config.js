13cbdaab13d8c6d3056b3fa7a8e80d355a5aa309
Mark Donatelli <markwdonatelli@yahoo.com>
2020-07-25 12:35:19 -0400

    global scss intitial setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['@/assets/scss/style.scss'],
   /*
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
@@ -50,10 +50,13 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: [],
+  modules: ['@nuxtjs/style-resources'],
   /*
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {},
+  styleResources: {
+    scss: ['~/assets/scss/variables.scss'],
+  },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container-full-width">
     <div class="row">
       <div class="header">
         <h1>Mark Donatelli</h1>
@@ -12,21 +12,14 @@
 export default {}
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .row {
   display: -ms-flexbox;
   display: flex;
   -ms-flex-wrap: wrap;
   flex-wrap: wrap;
+
   margin-right: -15px;
   margin-left: -15px;
-}
-.container {
-  max-width: 1300px;
-  width: 100%;
-  margin: 0 auto;
-  padding-left: 15px;
-  padding-right: 15px;
-  background-color: red;
 }
 </style>
