8908a83ec6cf8d25968893deb24fff873973e7ac
Mark Donatelli <markwdonatelli@yahoo.com>
2020-07-26 10:54:53 -0400

    coming soon landing page updated

--- components/ComingSoon.vue
+++ components/ComingSoon.vue
@@ -1,18 +1,30 @@
 <template>
-  <div>
-    <h1>Coming Soon...</h1>
-    <p>New portfolio launch!</p>
-    <p>Mark Donatelli - Front End Developer</p>
-    <p>
-      Contact -
-      <a href="mailto: markdonatelli3@gmail.com">markdonatelli3@gmail.com</a>
-      <a
-        href="https://www.linkedin.com/in/mark-donatelli-747194133/"
-        target="_blank"
-      >
-        Linkedin
-      </a>
-    </p>
+  <div class="container--inner">
+    <div class="main-card">
+      <h1>Coming Soon...</h1>
+      <p>Mark Donatelli - Front End Developer</p>
+      <div class="links">
+        <a href="mailto: markdonatelli3@gmail.com" class="icon-gmail">
+          <img src="../assets/img/icon-gmail.png" class="img-responsive" />
+        </a>
+
+        <a
+          href="https://github.com/MarkDonatelli"
+          class="icon-github"
+          target="_blank"
+        >
+          <img src="../assets/img/icon-github.png" class="img-responsive" />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/mark-donatelli-747194133/"
+          target="_blank"
+          class="icon-linkedin"
+        >
+          <img src="../assets/img/icon-linkedin.png" class="img-responsive" />
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,7 +39,6 @@ export default {
     return {
       title: this.title,
       meta: [
-        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
         {
           hid: 'description',
           name: 'description',
@@ -39,8 +50,171 @@ export default {
 }
 </script>
 
-<style>
-div {
+<style lang="scss" scoped>
+.container--inner {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 100vh;
+}
+
+.main-card {
+  position: relative;
+
+  width: 100%;
+  max-width: 786px;
+  padding: 100px 15px;
+
+  -webkit-transition: all 0.5s ease-out;
+  transition: all 0.5s ease-out;
   text-align: center;
+
+  border-radius: 5px;
+  background-color: white;
+  -webkit-box-shadow: 0 25px 20px -20px rgba(0, 0, 0, 0.3),
+    0 0 15px rgba(0, 0, 0, 0.06);
+  box-shadow: 0 25px 20px -20px rgba(0, 0, 0, 0.3), 0 0 15px rgba(0, 0, 0, 0.06);
+
+  &::before {
+    position: absolute;
+    z-index: -1;
+    top: -48px;
+    left: -48px;
+
+    display: block;
+
+    width: 100%;
+    height: 100%;
+
+    content: '';
+
+    border-radius: 5px;
+    background: linear-gradient(to bottom right, #1cf196 0%, #36a6bf 100%);
+    background: linear-gradient(to bottom right, #eb3349 0%, #f45c43 100%);
+    background: linear-gradient(to bottom right, #43cea2 0%, #185a9d 100%);
+  }
+
+  h1 {
+    text-transform: uppercase;
+
+    color: $black;
+
+    font-size: clamp(var(--minFontSize), var(--scaler), var(--maxFontSize));
+    font-weight: 800;
+
+    --minFontSize: 2rem;
+    --maxFontSize: 5rem;
+    --scaler: 10vw;
+  }
+
+  p {
+    letter-spacing: -2px;
+    text-transform: uppercase;
+
+    color: $black;
+
+    font-family: $titillium;
+    font-size: clamp(var(--minFontSize), var(--scaler), var(--maxFontSize));
+    font-weight: 700;
+
+    --minFontSize: 1rem;
+    --maxFontSize: 2rem;
+    --scaler: 5vw;
+  }
+}
+
+.links {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  max-width: 315px;
+  margin: 20px auto 0;
+  padding: 0 15px;
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 70px;
+    height: 70px;
+
+    cursor: pointer;
+
+    border-radius: 50%;
+    box-shadow: 0 25px 20px -20px rgba(67, 206, 162, 0.3),
+      0 0 15px rgba(24, 90, 157, 0.06);
+
+    &:hover,
+    :active,
+    :focus {
+      -webkit-animation-name: float, hang-and-float;
+      animation-name: float hang-and-float;
+      -webkit-animation-duration: 0.3s, 1.5s;
+      animation-duration: 0.3s, 1.5s;
+      -webkit-animation-timing-function: ease-out, ease-in-out;
+      animation-timing-function: ease-out, ease-in-out;
+      -webkit-animation-delay: 0s, 0.3s;
+      animation-delay: 0s, 0.3s;
+      -webkit-animation-iteration-count: 1, infinite;
+      animation-iteration-count: 1, infinite;
+      -webkit-animation-direction: normal, alternate;
+      animation-direction: normal, alternate;
+
+      -webkit-animation-fill-mode: forwards;
+      animation-fill-mode: forwards;
+    }
+  }
+
+  img {
+    max-width: 35px;
+  }
+}
+
+//keyframes
+
+// hover icon float
+@-webkit-keyframes hang-and-float {
+  0% {
+    -webkit-transform: translateY(-6px);
+    transform: translateY(-6px);
+  }
+  50% {
+    -webkit-transform: translateY(-4px);
+    transform: translateY(-4px);
+  }
+  100% {
+    -webkit-transform: translateY(-6px);
+    transform: translateY(-6px);
+  }
+}
+@keyframes hang-and-float {
+  0% {
+    -webkit-transform: translateY(-6px);
+    transform: translateY(-6px);
+  }
+  50% {
+    -webkit-transform: translateY(-4px);
+    transform: translateY(-4px);
+  }
+  100% {
+    -webkit-transform: translateY(-6px);
+    transform: translateY(-6px);
+  }
+}
+@-webkit-keyframes float {
+  100% {
+    -webkit-transform: translateY(-6px);
+    transform: translateY(-6px);
+  }
+}
+@keyframes float {
+  100% {
+    -webkit-transform: translateY(-6px);
+    transform: translateY(-6px);
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="container-full-width">
-    <div class="inner">
-      <ComingSoon />
-    </div>
+  <div class="container-full-width container-full-width--coming-soon">
+    <ComingSoon />
   </div>
 </template>
 
@@ -10,4 +8,28 @@
 export default {}
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.container-full-width--coming-soon {
+  position: relative;
+  z-index: 0;
+
+  overflow: hidden;
+
+  background-color: white;
+
+  &::before {
+    position: absolute;
+    z-index: -1;
+    top: 50%;
+    left: 50%;
+
+    width: 120vw;
+    height: 100%;
+
+    content: '';
+    transform: translate(-50%, -50%) rotate(30deg) skew(30deg);
+
+    background: #f5f5f5;
+  }
+}
+</style>
