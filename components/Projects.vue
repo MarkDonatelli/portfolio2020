f13055eb0346625f4e8e8feca8415133f7e6e6b8
Mark Donatelli <markwdonatelli@yahoo.com>
2020-10-10 16:16:47 -0400

    bug fixes

--- components/Projects.vue
+++ components/Projects.vue
@@ -227,26 +227,30 @@ export default {
       },
     })
 
-    gsap.utils.toArray('.grid-row').forEach((item) => {
-      gsap.fromTo(
-        item,
-        {
-          autoAlpha: 0,
-          y: 35,
-        },
-        {
-          scrollTrigger: {
-            trigger: item,
+    ScrollTrigger.matchMedia({
+      '(min-width: 769px)'() {
+        gsap.utils.toArray('.grid-row').forEach((item) => {
+          gsap.fromTo(
+            item,
+            {
+              autoAlpha: 0,
+              y: 35,
+            },
+            {
+              scrollTrigger: {
+                trigger: item,
 
-            once: true,
-          },
-          duration: 1,
-          autoAlpha: 1,
-          y: 0,
-          delay: 0.2,
-          ease: Power2.easeOut,
-        }
-      )
+                once: true,
+              },
+              duration: 1,
+              autoAlpha: 1,
+              y: 0,
+              delay: 0.2,
+              ease: Power2.easeOut,
+            }
+          )
+        })
+      },
     })
 
     const tl = gsap.timeline({ paused: true })
