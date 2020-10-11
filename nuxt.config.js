292abb58f5857a0d9944fccefb84e0903759ef90
Mark Donatelli <markwdonatelli@yahoo.com>
2020-10-11 10:03:03 -0400

    viewport scale issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       {
-        name: `'viewport'`,
+        name: 'viewport',
         content: 'width=device-width, user-scalable=no,  initial-scale=1',
       },
       {
