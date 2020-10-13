431903ca9973d1b952b586dcab66c349eecfb608
Mark Donatelli <markwdonatelli@yahoo.com>
2020-10-13 11:26:00 -0400

    functionns

---
+++ firebase/credentials.js
@@ -0,0 +1,12 @@
+export default {
+  config: {
+    apiKey: 'AIzaSyBvpLAS8GZTikkp1EYDTRwLLi-SUlDjx9E',
+    authDomain: 'my-portfolio-4d79e.firebaseapp.com',
+    databaseURL: 'https://my-portfolio-4d79e.firebaseio.com',
+    projectId: 'my-portfolio-4d79e',
+    storageBucket: 'my-portfolio-4d79e.appspot.com',
+    messagingSenderId: '678217505472',
+    appId: '1:678217505472:web:7debea10a242434680ab25',
+    measurementId: 'G-EZSKSBZZKM',
+  },
+}
